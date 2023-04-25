4c3f7b35254f777d1b2a54318c3c2d46fc4a333a
RuFiripo <127602538+RuFiripo@users.noreply.github.com>
2023-04-25 19:56:28 +0200

    Update Phitagor.py

--- Phitagor.py
+++ Phitagor.py
@@ -1,33 +1,72 @@
+import math
+import time
+
 while True:
-    RES = input('Téorema(1) ou Réciproque(2): ')
 
-    if RES == '1':
-            X1: float = float(input('Côté opposé: '))
-            X2 = float(input('côté adjacent: '))
+    print("           I \                                       ")
+    print("           I   \                                     ")
+    print("       3   I     \   1                               ")
+    print("           I       \                                 ")
+    print("           I_________\                               ")
+    print("               2                                     ")
 
-            P0 = (X2 ** 2 + X1 ** 2) ** (1 / 2)
+    print("De quel côté veux-tu calculer ?")
+    print("1 - hypoténuse")
+    print("2 - côté adjacent")
+    print("3 - Côté opposé")
 
-            print("....................................................")
+    RES = int(input('Saisissez l option souhaitée: '))
 
-            print("O valor da hypoténuse é: {}".format(P0))
+    if RES == 1:
 
-    elif RES == '2':
+        X1 = float(input('Côté opposé: '))
+        X2 = float(input('côté adjacent: '))
 
-            X1: float = float(input('Côté opposé: '))
-            X2 = float(input('hypoténuse: '))
+        P0 = math.sqrt( X2 ** 2 + X1 ** 2)
 
-            P0 = (X2 ** 2 - X1 ** 2) ** (1/2)
+        print("....................................................")
 
-            print("....................................................")
+        print("O valor da hypoténuse é: {}".format(P0))
+        time.sleep(5)
 
-            print("La valeur de l'hypoténuse est: {}".format(P0))
+    elif RES == 2:
 
+        X1: float = float(input('Côté opposé: '))
+        X2 = float(input('hypoténuse: '))
+
+        P0 = math.sqrt(X2 ** 2 - X1 ** 2)
+
+        print("....................................................")
+
+        print("La valeur de l'hypoténuse est: {}".format(P0))
+        time.sleep(5)
+
+    elif RES == 3:
+
+        X1: float = float(input('Côté adjacent: '))
+        X2 = float(input('hypoténuse: '))
+
+        P0 = math.sqrt(X2 ** 2 - X1 ** 2)
+
+        print("....................................................")
+
+        print("La valeur de l'hypoténuse est: {}".format(P0))
+        time.sleep(5)
+        
     else:
-            Souo = str(input('réponse invalide voulez-vous réessayer (O/N)'))
-            if Souo =='O':
-                    continue
-            elif Souo =='N':
-                    break
-            else:
-                    print("option invalide!")
+        print('réponse invalide voulez-vous réessayer')
+        print("1 - Oui")
+        print("2 - No")
 
+        Souo = int(input(" "))
+
+        if Souo == 1:
+                continue
+
+        elif Souo == 2:
+                break
+                time.sleep(2)
+        else:
+                print("option invalide!")
+                print("Est seulement possible de répondre avec un numéro")
+                time.sleep(4)
